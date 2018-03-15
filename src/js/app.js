220df07210a0981a0115f08a25b5ca26b80e3ed5
Pop Alexandra Maria <alexandra.p21m@gmail.com>
2018-03-15 11:40:02 +0200

    Set correct email address.

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,4 @@ import "../css/main.css";
 
 const container = document.querySelector( ".main" );
 
-container.innerHTML = "<h1>Hello from webpack !</h1>";
+container.innerHTML = "<h1>Hello from webpack starter app!</h1>";
